31fcc4127a2788624adcd0a28a24411c4a763a0d
Filipi Firmino <contatoic3web@gmail.com>
2019-02-05 07:53:01 -0300

    Alterações no background e responsividade do front-end

---
+++ asset/scripts/conectardb.php
@@ -0,0 +1,13 @@
+<?php
+
+    public function conectadb(){
+        $database = "meupersonal" ;
+        $user = "root";
+        $pass = '';
+        $ind = "localhost";
+
+        $con = new PDO("mysql:host=$ind; dbname=$database" ,$user , $pass);
+        
+    
+    
+    }
