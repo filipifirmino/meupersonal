aa738ad6c688561fd16e9d12c1317edafc0c258d
Filipi Firmino <contatoic3web@gmail.com>
2019-02-12 15:48:36 -0300

    Remanscentes da ultima atualização

--- asset/scripts/cadastra.php
+++ asset/scripts/cadastra.php
@@ -1,13 +1,14 @@
 <?php
     require conectardb.php;
-
+    conectardb();
     
     $nome = isset(_POST['name_user']);
     $email = isset(_POST['email_user']);
     $pass = isset(_POST['pass_user']);
 
-    conectardb();
-    
-    $query = INSERT INTO usuario ($nome,$email,$senha) VALUE (?,?,?);
+    try{
 
-?>
+    }catch (Exception $e){
+
+    }
+    
