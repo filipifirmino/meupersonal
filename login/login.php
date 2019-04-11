aee2cc0374589dfd00b4630e36795193c9a7901f
Filipi Firmino <contatoic3web@gmail.com>
2019-04-11 11:23:57 -0300

    bibliotecas locais erro modal no login permanece

--- login/login.php
+++ login/login.php
@@ -159,19 +159,16 @@
 	  
 			<div class="modal-body">
 			  <form action = "../asset/script/pdoInsert.php"  method= "POST">
-				  <div class="form-group ">
+				  <div class="form-group">
 					  <input class="form-control" type="name" name="name-user" placeholder =  "Nome Completo" require/>
 				  </div>
-				  <div class="form-group ">
+				  <div class="form-group">
 					  <input class="form-control" type="email" name="email-user" placeholder =  "e-mail" require/>
 				  </div>
-				  <div class="form-group ">
+				  <div class="form-group">
 					  <input class="form-control" type="password" name="pass-user" placeholder =  "Senha" require/>
 				  </div>    
-				  <!--<div class="form-group ">
-					  <input class="form-control" type="password" name="re-pass-user" placeholder =  "Repita Senha" require/>
-				  </div>-->
-					<div class="form-group ">
+					<div class="form-group">
 					  <input class="form-control" type="text" name="fone-user" placeholder =  "Ex: (99)-9 9999.9999" require/>
 				  </div>
 				  <button class = "btn btn-success" onclick = cadastrar() >Cadastrar</button>
@@ -188,10 +185,10 @@
 
 
 
-<script type="text/javascript" src="../asset/script/action.js"></script> <!--Script interno-->
+<script src="../asset/script/js/action.js"></script> <!--Script interno-->
 <!--Scripts do bootstrap-->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="../node_modules/jquery/dist/jquery.slim.min.js" ></script>
+<script src="../node_modules/popper.js/dist/umd/popper.min.js"></script>
+<script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
