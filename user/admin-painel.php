c1865784e5c89195e7e0fa1f0e6c0c97cdd417db
Filipi Firmino <contatoic3wen@gmail.com>
2019-04-17 11:43:45 -0300

    Adicionado funcionalidade de cadastro de turma

--- user/admin-painel.php
+++ user/admin-painel.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="authot" content="Ic3Web - CEO - Filipi Firmino">
     <meta name="description" content="Descrição breve da aplicação">
@@ -65,6 +65,8 @@
             </div>
                 
             <div class="dados">
+
+                <h3 class="title">Seus alunos</h3>
                 
                 <table class="table">
                 
@@ -77,10 +79,11 @@
                 </tr>             
                 
                 <?php
-
+                   
                     function score($contador){
                         for($i=1; $i<= $contador; $i++){
-                            echo '<img src="../asset/image/padm/star.svg" class="star">';
+                            return '<img src="../asset/image/padm/star.svg" class="star">';
+                            
                         }
                     }
 
@@ -103,6 +106,89 @@
                 <button class="btn btn-success" data-toggle="modal" data-target = "#modalCadastro"> Novo </button>
               </div>
             
+            <div class="turmas">
+                     <h3 class="title">Suas Turmas</h3>
+                <table class="table">
+
+                        <tr>
+                            <th>Turma</th>
+                            <th>Horario</th>
+                            <th>Alunos</th>
+                            <th>Status</th>
+                            <th>Ações </th>
+                        </tr>
+                    <?
+                        function validaTurma($par){
+                            if($par == 1){
+                                return  'Ativo';
+                            }else{
+                                return  'inativo';
+                            }
+                        }
+
+                        require '../asset/script/php/pdoDBConect.php';
+                        $sql1 = "SELECT * FROM turmas";
+                        $sql1 = $pdo->query($sql1);
+                        if($sql1->rowCount() > 0){
+                            foreach($sql1->fetchAll() as $turmas){
+                                echo '<tr>';
+                                echo '<td>'.$turmas['id'].'</td>';
+                                echo '<td>'.$turmas['horario'].'</td>';
+                                echo '<td>'.$turmas['qtdAlunos'].'</td>';
+                                echo '<td>'.validaTurma($turmas['estatus']).'</td>';
+                                echo '<td> <a href="editarTurmas.php?id="'.$turmas['id'].'">Editar</a> - <a href="excluirTurmas.php?id="'.$turmas['id'].'">Excluir</a></td>';
+
+                            }
+                        }
+
+                    ?>
+
+                </table>
+                <button class="btn btn-success" data-toggle="modal" data-target="#modalTurma"> Novo</button>
+            </div>
+
+            <!-- Modal cadastro de turmas-->
+            <div class="modal fade" id="modalTurma" tabindex="-1" role="dialog" arial-labelledby="modalTurma" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+		        <div class="modal-content">
+			        <div class="modal-header">
+			            <h5 class="modal-title" id="modalCadastroLabel">Cadastro de Turmas</h5>
+			    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				    <span aria-hidden="true">&times;</span>
+			     </button>
+			</div>
+	  
+			<div class="modal-body">
+			  <form action = "pdoInsert.php"  method= "POST">
+				  <div class="form-group">
+					  <input class="form-control" type="name" name="name-turma" placeholder =  "Nome da turma" require/>
+				  </div>
+				  <div class="form-group">
+					  <input class="form-control" type="text" name="horario-turma" placeholder =  "horario" require/>
+				  </div>
+				  <div class="form-group">
+					  <input type="text" class="form-control" name="aluno-turma" placeholder="Adcione alunos">
+				  </div>
+                  <div class="form-group">
+					  <select class="form-control" name="status-turma">
+                        <option value="1">Ativa</option>
+                        <option value="0">Desativada</option>
+                      </select>
+				  </div>     
+					
+				  <button class = "btn btn-success" onclick = cadastrar() >Cadastrar</button>
+			  </form>
+			</div>
+			<div class="modal-footer">
+			  <button type="submit" class="btn btn-secondary" data-dismiss="modal">Close</button>
+			 </div>
+			</div>
+		</div>
+	</div>  
+
+
+
+
             <!-- Modal cadastro-->
             <div class="modal fade" id="modalCadastro" tabindex="-1" role="dialog" aria-labelledby="modalCadastro" aria-hidden="true">
 		<div class="modal-dialog" role="document">
---
+++ user/pdoInsert.php
@@ -0,0 +1,32 @@
+<?php
+$dsn = "mysql:dbname=meupersonal; host=localhost";
+$dbuser = "root";
+$dbpass = "dijital123";
+
+    try{
+
+        $pdo = new PDO($dsn, $dbuser, $dbpass);
+            /*Conectando ao banco de dados*/
+
+            /* Recebendo dados do formulario */
+            $nome = $_POST['name-turma'];
+            $horario = $_POST['horario-turma'];
+            $aluno = $_POST['aluno-turma'];
+            $status = $_POST['status-turma'];
+
+    
+        $sql = "INSERT INTO turmas SET nome = '$nome', horario = '$horario', qtdAluno = '$aluno', estatus ='$status'";
+        /*Preparando a query de iserção de dados*/ 
+        $sql = $pdo->query($sql);
+        /*Inserção dos dados no banco*/
+        $pdo->lastInsertId(); /*retorna o id do usuario*/
+        
+        header('Location: http://localhost/meupersonal/user/admin-painel.php');
+        
+    } catch(PDOException $e){
+        echo "Erro: " . $e->getMessage();
+    }
+
+
+
+?>
